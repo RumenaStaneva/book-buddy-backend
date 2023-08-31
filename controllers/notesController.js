f0b27f150a61cd1e22c91f381a16a6ebcd634b29
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-08-31 20:50:36 +0300

    implemented infinate scroll logic for notes

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -9,7 +9,7 @@ const createNote = async (req, res) => {
     const userId = req.user._id;
     const { bookId, noteText } = req.body;
     try {
-        console.log(userId, bookId, noteText);
+        // console.log(userId, bookId, noteText);
         const note = await NoteModel.createNote({ bookId, userId, noteText });
         res.status(200).json({ note });
     } catch (error) {
@@ -20,18 +20,25 @@ const createNote = async (req, res) => {
 const getNotes = async (req, res) => {
     const userId = req.user._id;
     const bookId = req.query.bookId;
+    const offset = parseInt(req.query.offset) || 0;
+    const limit = 10;
+
     try {
         const book = await BookModel.findOne({ owner: userId, _id: bookId });
         if (!book) {
             return res.status(400).json({ error: 'Book does not exist' });
         }
-        const notes = await NoteModel.find({ userId, bookId });
-        console.log(notes);
+
+        const notes = await NoteModel.find({ userId, bookId })
+            .skip(offset)
+            .limit(limit);
+
         res.status(200).json({ notes });
     } catch (error) {
-        res.status(400).json({ error: 'Error while book`s notes' });
+        res.status(400).json({ error: 'Error while fetching book\'s notes' });
     }
-}
+};
+
 
 module.exports = {
     createNote,
