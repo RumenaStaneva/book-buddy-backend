2d3ada6990ad3e0985790b8df8b366c8397c9dbc
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-10-02 11:11:21 +0300

    removed swagger and addeed google signup/login

--- routes/user.ts
+++ routes/user.ts
@@ -1,138 +1,15 @@
 import { Router } from 'express';
-// const { loginUser, signUpUser, signUpAdmin, getProfile, updateProfile, verifyUser, forgotPassword, resetPassword } = require('../controllers/userController');
-import { loginUser, signUpUser, signUpAdmin, getProfile, updateProfile, verifyUser, forgotPassword, resetPassword } from '../controllers/userController'
+import { loginUser, signUpUser, signUpAdmin, getProfile, updateProfile, verifyUser, forgotPassword, resetPassword, loginWithGoogle, signupWithGoogle } from '../controllers/userController'
 import requireAuth from '../middleware/requireAuth';
 
 const router: Router = Router();
 
 router.get('/verify/:token', verifyUser);
-
-/**
- * @swagger
- * tags:
- *   name: Users
- *   description: API endpoints for user management
- */
-
-/**
- * @swagger
- * /api/users/login:
- *   post:
- *     summary: Log in a user
- *     tags: [Users]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *                 description: The email address of the user.
- *               password:
- *                 type: string
- *                 description: The password of the user.
- *             required:
- *               - email
- *               - password
- *     responses:
- *       '200':
- *         description: Successful login
- *       '401':
- *         description: Unauthorized
- *         content:
- *           application/json:
- *             example:
- *               message: Incorrect email or password
- *       '500':
- *         description: Internal server error
- */
+router.post('/signup-with-google', signupWithGoogle)
+router.post('/login-with-google', loginWithGoogle)
 router.post('/login', loginUser);
-
-/**
- * @swagger
- * /api/users/sign-up:
- *   post:
- *     summary: Sign up a new user
- *     tags: [Users]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *                 description: The email address of the user.
- *               password:
- *                 type: string
- *                 description: The password of the user.
- *             required:
- *               - email
- *               - password
- *     responses:
- *       '201':
- *         description: User registered successfully
- *       '400':
- *         description: Bad request
- *         content:
- *           application/json:
- *             example:
- *               message: Weak password. Password must be at least 8 characters long and contain a mix of letters, numbers, and special characters.
- *       '409':
- *         description: Conflict
- *         content:
- *           application/json:
- *             example:
- *               message: Email already in use
- *       '500':
- *         description: Internal server error
- */
 router.post('/sign-up', signUpUser);
-/**
- * @swagger
- * /api/users/sign-up-admin:
- *   post:
- *     summary: Sign up a new admin user
- *     tags: [Users]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *                 description: The email address of the admin user.
- *               password:
- *                 type: string
- *                 description: The password of the admin user.
- *             required:
- *               - email
- *               - password
- *     responses:
- *       '201':
- *         description: Admin user registered successfully
- *       '400':
- *         description: Bad request
- *         content:
- *           application/json:
- *             example:
- *               message: Weak password. Password must be at least 8 characters long and contain a mix of letters, numbers, and special characters.
- *       '409':
- *         description: Conflict
- *         content:
- *           application/json:
- *             example:
- *               message: Email already in use
- *       '500':
- *         description: Internal server error
-*/
 router.post('/sign-up-admin', signUpAdmin);
-
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
 
