e592a20208274f1fa5a5faa6712c2e11592f162f
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-10-24 11:05:58 +0300

    made possible to attach files on books

--- routes/books.ts
+++ routes/books.ts
@@ -2,6 +2,16 @@ import { Request, Response, Router } from 'express';
 import { getUserLibrary, addToShelf, getAllBooksOnShelf, updateBook, getBookDetails, deleteBook } from '../controllers/booksController';
 import requireAuth from '../middleware/requireAuth';
 
+import multer from 'multer';
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 10 * 1024 * 1024, // 10 MB 
+        fieldSize: 10 * 1024 * 1024, // 10 MB
+    },
+});
+
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response) => {
@@ -13,10 +23,10 @@ router.use(requireAuth);
 //require auth for all books routes
 router.get('/library', getUserLibrary)
 
-router.post('/add-to-shelf', addToShelf);
+router.post('/add-to-shelf', upload.single('thumbnail'), addToShelf);
 
 router.get('/see-all', getAllBooksOnShelf);
-router.put('/update-book', updateBook);
+router.put('/update-book', upload.single('thumbnail'), updateBook);
 router.get('/book-details', getBookDetails);
 router.delete('/delete-book', deleteBook);
 
