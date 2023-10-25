128b97f07d7f031f37a3995c372eed558d906077
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-10-25 17:17:31 +0300

    validation for adding screen time

--- controllers/timeSwapController.ts
+++ controllers/timeSwapController.ts
@@ -11,7 +11,8 @@ import {
     convertToMMDDFormat,
     updateReadingTime,
     calculateReadingTimeSpendOnBook,
-    updateBookReadDuringDay
+    updateBookReadDuringDay,
+    validateScreenTimeData
 } from '../utils/timeSwap'
 
 const getCurrentWeekDates = async (req: IGetUserAuthInfoRequest, res: Response) => {
@@ -61,6 +62,9 @@ const saveTime = async (req: IGetUserAuthInfoRequest, res: Response) => {
     }
     try {
         const screenTimeData = req.body;
+        if (!validateScreenTimeData(screenTimeData)) {
+            return res.status(400).json({ error: 'Invalid screen time data' });
+        }
         const startDate = parse(screenTimeData[0].date, 'yyyy/MM/dd', new Date());
         const startOfWeekDay = startOfWeek(addWeeks(startDate, 1), { weekStartsOn: 2 });
         const lastWeekEnd = endOfWeek(addWeeks(startDate, 1), { weekStartsOn: 2 });
