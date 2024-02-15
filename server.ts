f09a3f08e4bc8f4fe07799504ba48b8e1ef60392
RumenaStaneva <rumenastaneva2106@gmail.com>
2024-02-15 14:29:38 +0200

    fixing cors error

--- server.ts
+++ server.ts
@@ -1,61 +1,69 @@
-import express, { Express, Application, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bookRoutes from './routes/books';
-import userRoutes from './routes/user';
-import apiRoutes from './routes/api';
-import noteRoutes from './routes/notes';
-import timeSwapRoutes from './routes/timeSwap';
-import spotifyRoutes from './routes/spotify';
-import tokenCleanup from './tokenCleanup';
-import mongoose from 'mongoose';
-import swaggerUi from 'swagger-ui-express';
-import { loginWithGoogle } from './controllers/userController';
-import swaggerSpec from './swagger';
+import express, { Express, Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bookRoutes from "./routes/books";
+import userRoutes from "./routes/user";
+import apiRoutes from "./routes/api";
+import noteRoutes from "./routes/notes";
+import timeSwapRoutes from "./routes/timeSwap";
+import spotifyRoutes from "./routes/spotify";
+import tokenCleanup from "./tokenCleanup";
+import mongoose from "mongoose";
+import swaggerUi from "swagger-ui-express";
+import { loginWithGoogle } from "./controllers/userController";
+import swaggerSpec from "./swagger";
 import path from "path";
 
 const app: Application = express();
 
-
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://book-buddy.tech"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+
+//   app.use(cors());
 // app.use(express.urlencoded({ extended: false }));
 
 let PORT: string | number = process.env.PORT || 5000;
-let MONGO_URI: string = process.env.MONGO_URI || '';
-app.use(express.static(path.join(__dirname, "../client/build")))
+let MONGO_URI: string = process.env.MONGO_URI || "";
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(
-    "/book-buddy-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, { explorer: true })
+  "/book-buddy-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, { explorer: true })
 );
 
 //middleware
 app.use((req: Request, res: Response, next) => {
-    next();
+  next();
 });
 
-app.use('/spotify', spotifyRoutes);
-app.use('/users', userRoutes);
-app.use('/api', apiRoutes);
-app.use('/books', bookRoutes);
-app.use('/notes', noteRoutes);
-app.use('/time-swap', timeSwapRoutes);
+app.use("/spotify", spotifyRoutes);
+app.use("/users", userRoutes);
+app.use("/api", apiRoutes);
+app.use("/books", bookRoutes);
+app.use("/notes", noteRoutes);
+app.use("/time-swap", timeSwapRoutes);
 
-app.get('/', (req, res) => {
-    res.type('text/html');
-    res.send('<h1>Hi there!</h1>');
-})
+app.get("/", (req, res) => {
+  res.type("text/html");
+  res.send("<h1>Hi there!</h1>");
+});
 
-const host = '0.0.0.0';
-mongoose.connect(MONGO_URI)
-    .then(() => {
-        app.listen(Number(PORT), host, () => {
-            console.log('Connected to db & listening on port:', PORT)
-        })
-    })
-    .catch((error: Error) => {
-        console.log(error)
-    })
+const host = "0.0.0.0";
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(Number(PORT), host, () => {
+      console.log("Connected to db & listening on port:", PORT);
+    });
+  })
+  .catch((error: Error) => {
+    console.log(error);
+  });
